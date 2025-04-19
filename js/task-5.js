bdff400dfdc7044ae96552d11ea95dee22b9ff32
Iryna Buhela <developer.buhela@gmail.com>
2025-04-19 14:03:51 +0300

    hw7

--- js/task-5.js
+++ js/task-5.js
@@ -6,3 +6,19 @@
 // в той час, як колір фону на < body > буде у форматі rgb.Це нормально й 
 // не потребує якихось правок.
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function changeColorBtn() {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  span.textContent = newColor;
+}
+
+const button = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
+button.addEventListener("click", changeColorBtn);
